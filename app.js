126a35fab0d20f9e54553309125afc31c196b8f5
José Tomás Albornoz <jojo@eljojo.net>
2012-11-24 04:57:23 +0100

    now upload pictures and shit in the node server

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.configure(function(){
   app.set('view engine', 'ejs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(express.bodyParser());
+  app.use(express.bodyParser({keepExtensions: true, uploadDir: __dirname + "/public/uploads"}));
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
