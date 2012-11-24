15319e0311b24767439303d046ed866910aad23d
Jon Volkmar <jonathonvolkmar@gmail.com>
2012-11-24 13:17:19 +0100

    MultiBulge (c) has arrived.

--- public/facebulge.js
+++ public/facebulge.js
@@ -1,6 +1,19 @@
+// Parameters for bulge algorithm
 var x_thin = 1.25;
 var bulge_factor = 0.5;
 var bulge_boost = 10;
+var face_radius_scale = 0.8;
+var grid_width = 50;
+var grid_height = 50;
+
+// Global OpenGL objects
+var camera,
+    renderer,
+    scene,
+    material,
+    mesh,
+    texture,
+    geometry;
 
 function start() {
 	
@@ -26,32 +39,33 @@ function euclid_distance(x1, y1, x2, y2)
 	return Math.sqrt(x * x + y * y);
 }
 
-function makeBulgeGridGeometry(width, height, divWidth, 
-		divHeight, bulgeCX, bulgeCY, bulgeRadius)
+function addBulge(geom, cx, cy, radius)
 {
-	var geom = new THREE.PlaneGeometry(width, height, divWidth, divHeight);
-	// find square of points to consider
-	var divCX = Math.round(((bulgeCX * 1.0) / width) * divWidth);
-	var divCY = Math.round(((bulgeCY * 1.0) / height) * divHeight);
-	var divX1 = Math.floor(((bulgeCX-bulgeRadius *1.0) / width) * divWidth);
-	var divX2 = Math.ceil(((bulgeCX+bulgeRadius *1.0) / width) * divWidth);
-	var divY1 = Math.floor(((bulgeCY-bulgeRadius *1.0) / height) * divHeight);
-	var divY2 = Math.ceil(((bulgeCY+bulgeRadius *1.0) / height) * divHeight);
-
+	var divWidth = geom.widthSegments;
+	var divHeight = geom.heightSegments;
+	var width = geom.width;
+	var height = geom.height;
 	var divSizeX = width/(divWidth*1.0);
 	var divSizeY = height/(divHeight*1.0);
+	var divCX = Math.round(((cx * 1.0) / width) * divWidth);
+	var divCY = Math.round(((cy * 1.0) / height) * divHeight);
+	var divX1 = Math.floor(((cx -radius*1.0) / width) * divWidth);
+	var divX2 = Math.ceil(((cx+radius*1.0) / width) * divWidth);
+	var divY1 = Math.floor(((cy-radius*1.0) / height) * divHeight);
+	var divY2 = Math.ceil(((cy+radius*1.0) / height) * divHeight);
+
 	for (var i=divX1; i<=divX2; i++)
 	{
 		for (var j=divY1; j<=divY2; j++)
 		{
-			if (euclid_distance(i*divSizeX, j*divSizeY, bulgeCX, bulgeCY) > bulgeRadius)
+			if (euclid_distance(i*divSizeX, j*divSizeY, cx, cy) > radius)
 				continue;
 
 			var x = i * divSizeX;
 			var y = j * divSizeY;
-			var x_diff = (x - bulgeCX) * x_thin;
-			var y_diff = y - bulgeCY;
-			var z = Math.sqrt(bulgeRadius*bulgeRadius-x_diff*x_diff-y_diff*y_diff);
+			var x_diff = (x - cx) * x_thin;
+			var y_diff = y - cy;
+			var z = Math.sqrt(radius*radius-x_diff*x_diff-y_diff*y_diff);
 
 			var v = getVertex(i, j, geom);
 			if (v != undefined)
@@ -61,8 +75,6 @@ function makeBulgeGridGeometry(width, height, divWidth,
 
 		}
 	}
-
-	return geom;
 }
 
 
@@ -79,11 +91,6 @@ function loadScene() {
 	var face = face_info.faces[0]; // only use 1st face for now
 	var img_height = face_info.height;
 	var img_width = face_info.width;
-	var grid_width = 50;
-	var grid_height = 50;
-	var face_radius = 0.8 * (face.width+face.height)/2 ;
-	var face_cX = face.x + face.width/2;
-	var face_cY = face.y + face.height/2;
 	
 	renderer = new THREE.WebGLRenderer();
 	camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
@@ -92,17 +99,22 @@ function loadScene() {
 	texture = THREE.ImageUtils.loadTexture(photo_path, {}, function() {
 			// need to wait until texture is loaded to display stuff
             start();
-		}),
+		});
+	material = new THREE.MeshBasicMaterial({map: texture});
+	geometry= new THREE.PlaneGeometry(img_width, img_height, grid_width, grid_height);
 
-        material = new THREE.MeshBasicMaterial({map: texture}),
-		geometry = makeBulgeGridGeometry(img_width, 
-				img_height,
-				grid_width,
-				grid_height,
-				face_cX,
-				face_cY,
-				face_radius),
-        mesh = new THREE.Mesh(geometry, material);
+	if (face_info.numfaces > 0)
+	{
+		for (var i=0; i<face_info.faces.length; i++) {
+			var face = face_info.faces[i];
+			var face_cX = face.x + face.width/2;
+			var face_cY = face.y + face.height/2;
+			var face_radius = face_radius_scale * (face.width+face.height)/2 ;
+			addBulge(geometry, face_cX, face_cY, face_radius);
+		}
+	}
+	
+    mesh = new THREE.Mesh(geometry, material);
 	mesh.geometry.dynamic = true;
     var pointLight = new THREE.PointLight(0xFFFFFF);
 
@@ -123,7 +135,7 @@ function animate(t) {
 	// TODO parameterize
 	// period, angle_diff
 	var rotation = 1;
-	var period = 1000;
+	var period = 4000;
 	
 	var img_height = face_info.height;
 	var img_width = face_info.width;
