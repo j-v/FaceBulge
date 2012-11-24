ec144fd39b014fdf7890d1bc1e37d8fe675ffcb5
José Tomás Albornoz <jojo@eljojo.net>
2012-11-24 04:38:20 +0100

    moved server.js into express magical stuff. good stuff

--- routes/upload.js
+++ routes/upload.js
@@ -3,6 +3,20 @@
  * POST upload file.
  */
 
-exports.action = function(req, res){
-  res.send("respond with a resource");
+// __dirname will be /Users/jojo/code/FaceBulge/routes
+var facedetect_path = __dirname + '/../experiments/facedetect/'
+var exec = require('child_process').exec,
+    child;
+    
+exports.action = function(req, res) {
+  console.log('running: ' + 'cd '+ facedetect_path + ' && ./a.out ' + req.files.file.path)
+  child = exec('cd '+ facedetect_path + ' && ./a.out ' + req.files.file.path,
+    function (error, stdout, stderr) {
+      res.render('uploaded', { title: 'FaceBulge', face_info: stdout });
+      if (error !== null) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        console.log('exec error: ' + error);
+      }
+  });
 };
