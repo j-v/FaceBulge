3397a247058de73e769d2bd20d09c73546c00227
José Tomás Albornoz <jojo@eljojo.net>
2012-11-24 04:21:23 +0100

    server a little bit better

--- server.js
+++ server.js
@@ -4,20 +4,22 @@ var express = require('express')
 var exec = require('child_process').exec,
     child;
 
+//var facedetect_path = '/Users/jojo/code/FaceBulge/experiments/facedetect/'
+var facedetect_path = '/home/jojo/FaceBulge/experiments/facedetect/'
+
 app.configure(function() {
   var hourMs = 1000*60*60;
   app.use(express.logger());
   app.use(express.static(__dirname + '/experiments', { maxAge: hourMs }));
-  //app.use(express.directory(__dirname + '/experiments'));
+  // app.use(express.directory(__dirname + '/experiments'));
   app.use(express.errorHandler());
   app.post('/upload', express.bodyParser(), function(req, res) {
-    console.log('running: '+ '/home/jojo/FaceBulge/experiments/facedetect/a.out ' + req.files.file.path)
-    child = exec('/home/jojo/FaceBulge/experiments/facedetect/a.out ' + req.files.file.path,
+    child = exec('cd '+ facedetect_path + ' && ./a.out ' + req.files.file.path,
       function (error, stdout, stderr) {
         res.send(stdout);
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
         if (error !== null) {
+          console.log('stdout: ' + stdout);
+          console.log('stderr: ' + stderr);
           console.log('exec error: ' + error);
         }
     });
